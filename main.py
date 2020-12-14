f51d6b7dee49d5be290583c1236894c0a8d34f9c
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2020-12-13 16:19:42 -0800

    Add functions from key to main

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
 import argparse
 from database import Database, Permission
-from key import write_data_to_key, read_key
+import Rpi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
 
 
+READER = SimpleMFRC522()
 DATABASE = Database()
 
 
 def setup():
     DATABASE.create_table()
+
+
+def write_data_to_key(data: str):
+    did_complete = False
+    try:
+        READER.write(data)
+        print("Data written")
+        did_complete = True
+    finally:
+        GPIO.cleanup()
+
+    return did_complete
+
+
+def read_key():
+    did_complete = False
+    try:
+        id, text = READER.read()
+        did_complete = True
+    finally:
+        GPIO.cleanup()
+
+    if did_complete:
+        return id, text
 
 
 def create_key():
